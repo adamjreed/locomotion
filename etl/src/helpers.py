9ca03a324ebf69b73c92b25e2057781ec277bcdd
Adam Reed <adam@adamjreed.com>
2020-02-15 01:59:02 -0500

    added routes metadata to cities endpoint and refactored athena querying into a helper

--- etl/src/helpers.py
+++ etl/src/helpers.py
@@ -1,6 +1,7 @@
 import os
 import redis
 import json
+import boto3
 
 
 def load_redis(key, content):
@@ -8,6 +9,55 @@ def load_redis(key, content):
                     port=os.environ["REDIS_PORT"])
 
     return r.set(key, json.dumps(content))
+
+
+def query_athena(query):
+    athena = boto3.client("athena")
+
+    queryStart = athena.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            "Database": "locomotion"
+        },
+        ResultConfiguration={
+            "OutputLocation": "s3://%s/%s/" %
+            (os.environ["BUCKET_NAME"], "athena_result")
+        }
+    )
+
+    finalState = False
+
+    while finalState is not True:
+        queryExecution = athena.get_query_execution(
+            QueryExecutionId=queryStart["QueryExecutionId"]
+        )
+
+        finalState = check_query_result(
+            queryExecution["QueryExecution"]["Status"]["State"]
+        )
+
+    if queryExecution["QueryExecution"]["Status"]["State"] == "SUCCEEDED":
+        queryResults = athena.get_query_results(
+            QueryExecutionId=queryStart["QueryExecutionId"]
+        )
+        headers = None
+        parsedResults = []
+        for stop in queryResults["ResultSet"]["Rows"]:
+            if headers is None:
+                headers = []
+                for value in stop["Data"]:
+                    headers.append(value["VarCharValue"])
+            else:
+                i = 0
+                row = {}
+                for value in stop["Data"]:
+                    row[headers[i]] = value["VarCharValue"]
+                    i += 1
+                parsedResults.append(row)
+        return parsedResults
+    else:
+        raise "Error executing Athena query: %s" % \
+            queryExecution["QueryExecution"]["Status"]["State"]
 
 
 def check_query_result(status):
--- etl/src/stations.py
+++ etl/src/stations.py
@@ -1,61 +1,16 @@
-import os
-import boto3
 import argparse
-from helpers import load_redis, check_query_result
+from helpers import load_redis, query_athena
 
-athena = boto3.client("athena")
 parser = argparse.ArgumentParser()
 parser.add_argument('--city')
 args = parser.parse_args()
 
-query = """SELECT stop_id, stop_name, stop_lat, stop_lon
-        FROM %s_stops
-        WHERE stop_id > 40000
-        AND stop_id < 60000;"""
+query = """
+    SELECT stop_id, stop_name, stop_lat, stop_lon
+    FROM %s_stops
+    WHERE stop_id > 40000
+    AND stop_id < 60000;
+""" % (args.city)
 
-queryStart = athena.start_query_execution(
-    QueryString=(query % args.city),
-    QueryExecutionContext={
-        "Database": "locomotion"
-    },
-    ResultConfiguration={
-        "OutputLocation": "s3://%s/%s/" %
-        (os.environ["BUCKET_NAME"], args.city)
-    }
-)
-
-finalState = False
-
-while finalState is not True:
-    queryExecution = athena.get_query_execution(
-        QueryExecutionId=queryStart["QueryExecutionId"]
-    )
-
-    finalState = check_query_result(
-        queryExecution["QueryExecution"]["Status"]["State"]
-    )
-
-if queryExecution["QueryExecution"]["Status"]["State"] == "SUCCEEDED":
-    queryResults = athena.get_query_results(
-        QueryExecutionId=queryStart["QueryExecutionId"]
-    )
-    headers = None
-    parsedResults = []
-    for stop in queryResults["ResultSet"]["Rows"]:
-        if headers is None:
-            headers = []
-            for value in stop["Data"]:
-                headers.append(value["VarCharValue"])
-        else:
-            i = 0
-            row = {}
-            for value in stop["Data"]:
-                row[headers[i]] = value["VarCharValue"]
-                i += 1
-            parsedResults.append(row)
-
-    load_redis("stations_" + args.city, parsedResults)
-    print('{"success":true}')
-else:
-    raise "Error executing Athena query: %s" % \
-        queryExecution["QueryExecution"]["Status"]["State"]
+load_redis("stations_" + args.city, query_athena(query))
+print('{"success":true}')
